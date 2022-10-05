47fba40ef9a2590afb332149e9b3ecc063db325b
Arthur Fuzaro de Oliveira <afuzarodeoliveira@gmail.com>
2022-10-05 20:45:56 -0300

    Fixing unit test

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
+        self.assertEqual(self.result.data.decode('utf-8'), "Olá Grupo 15")
