e7ae012aa4fb7655d978cd85613adc415ed0ee53
Arthur Fuzaro de Oliveira <afuzarodeoliveira@gmail.com>
2022-11-26 13:49:13 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -9,15 +9,5 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
 
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
 if __name__ == '__main__':
     app.run()
